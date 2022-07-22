35f7bee7e6c10957f0face7ca6378ea3fb771bdf
MarianMoskal <77517344+MarianMoskal@users.noreply.github.com>
2022-07-22 08:53:16 +0200

    add filtering data & table styles

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ const destination = 'apname';
 const time = 'esti';
 const gate = 'gate';
 const remark = 'status';
+const name = 'alname';
 
-const sortedData = [...data].sort((a, b) =>
+const filteredData = data.filter(i => !i[name].includes('copy'));
+
+const sortedData = [...filteredData].sort((a, b) =>
   Date.parse(a[time]) < Date.parse(b[time]) ? -1 : (Date.parse(a[time]) > Date.parse(b[time]) ? 1 : 0));
-
 
 sortedData.map(e => {
   const row = document.createElement('tr');
@@ -22,8 +24,6 @@ sortedData.map(e => {
   const remarkTd = document.createElement('td');
   let timeValue;
 
-  // console.log(e[time]);
-
   if (e[time]) {
     const hours = new Date(Date.parse(e[time])).getHours();
     let minutes = new Date(Date.parse(e[time])).getMinutes();
@@ -35,14 +35,24 @@ sortedData.map(e => {
     timeValue = '-';
   };
 
-  // console.log(timeValue);
-
   terminalTd.appendChild(document.createTextNode(e[terminal] ? e[terminal] : '-'));
   flightTd.appendChild(document.createTextNode(e[flight] ? e[flight] : '-'));
+  flightTd.classList.add('flight-cell');
   destinationTd.appendChild(document.createTextNode(e[destination] ? e[destination] : '-'));
+  destinationTd.classList.add('destination-cell');
   timeTd.appendChild(document.createTextNode(timeValue));
   gateTd.appendChild(document.createTextNode(e[gate] ? e[gate] : '-'));
   remarkTd.appendChild(document.createTextNode(e[remark] ? e[remark] : '-'));
+  remarkTd.classList.add('remark-cell');
+  if (remarkTd.innerText === 'Boarding' || remarkTd.innerText === 'baggage delivery') {
+    remarkTd.classList.add('remark-yellow')
+  }
+  if (remarkTd.innerText === 'on position' || remarkTd.innerText === 'departed' || remarkTd.innerText === 'landed' || remarkTd.innerText === 'ready for Boarding') {
+    remarkTd.classList.add('remark-green');
+  }
+  if (remarkTd.innerText === 'closed') {
+    remarkTd.classList.add('remark-red')
+  }
 
   row.append(terminalTd, flightTd, destinationTd, timeTd, gateTd, remarkTd)
   
